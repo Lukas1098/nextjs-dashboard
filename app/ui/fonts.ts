1eee022ab5f940fcba80dc648ca23611590c5a88
Lucas Bernasconi <lucasbernasconi51@gmail.com>
2024-12-29 09:55:56 -0300

    Agrega el archivo fonts.ts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter} from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight:'400' });   //Importamos la fuente Inter de Google Fonts, y despues especificamos el subconjunto de caracteres que queremos usar.
